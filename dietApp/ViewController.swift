e5b384c55385b51554596a144230a9430d17f053
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-11-21 10:33:35 +0900

    Initial Commit

---
+++ dietApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dietApp
+//
+//  Created by 小倉瑞希 on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
